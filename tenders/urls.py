b81239ec605bf3d091d2973e0fcc99e9d1b66424
CoopNich <CoopNich92@gmail.com>
2020-06-15 17:04:45 -0500

    registration/login view added. urls.py updated

--- tenders/urls.py
+++ tenders/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
+from tendersapi.models import *
+from tendersapi.views import register_user, login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('register', register_user),
+    path('login', login_user),
     path('api-token-auth/', obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
