cc888ceaf5744a77794c655d80fcfccc2e5ead04
CoopNich <CoopNich92@gmail.com>
2020-06-16 10:58:30 -0500

    max_length added to all CharFields

--- tendersapi/models/cocktail.py
+++ tendersapi/models/cocktail.py
@@ -5,15 +5,15 @@ from .bartender import Bartender
 
 class Cocktail(models.Model):
     
-    external_id = models.CharField()
-    name = models.CharField()
+    external_id = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     bartender = models.ForeignKey(Bartender, on_delete=models.DO_NOTHING)
     date_added = models.DateTimeField(auto_now=True)
-    glass = models.CharField()
-    instructions = models.CharField()
+    glass = models.CharField(max_length=255)
+    instructions = models.CharField(max_length=255)
     is_edited = models.BooleanField()
     is_new = models.BooleanField()
-    image_url = models.CharField(null=True)
+    image_url = models.CharField(null=True, max_length=1000)
    
 
     def __str__(self):
--- tendersapi/models/ingredients.py
+++ tendersapi/models/ingredients.py
@@ -5,8 +5,8 @@ from .cocktail import Cocktail
 class Ingredient(models.Model):
 
     cocktail = models.ForeignKey(Cocktail, on_delete=models.DO_NOTHING)
-    ingredient = models.CharField()
-    measurement = models.CharField()
+    ingredient = models.CharField(max_length=50)
+    measurement = models.CharField(max_length=50)
     
     def __str__(self):
         return f'{self.measurement} of {self.ingredient}' 
