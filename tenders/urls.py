0e63c340f3d62b8c24129078d0f69982b053458c
CoopNich <CoopNich92@gmail.com>
2020-06-16 09:42:55 -0500

    original models/views deleted, packages made

--- tenders/urls.py
+++ tenders/urls.py
@@ -17,7 +17,7 @@ from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from tendersapi.models import *
-from tendersapi.views import register_user, login_user
+from tendersapi.views import *
 
 router = routers.DefaultRouter(trailing_slash=False)
 
