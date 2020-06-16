a15aa40e17fac2c8ea9a0a0d9a1cb8c2e43d59c2
CoopNich <CoopNich92@gmail.com>
2020-06-16 15:10:44 -0500

    image_url null value true in bartender model. set to null in register.py

--- tenders/settings.py
+++ tenders/settings.py
@@ -86,8 +86,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'tenders.wsgi.application'
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8080',
-    'http://127.0.0.1:8080'
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
 )
 
 
